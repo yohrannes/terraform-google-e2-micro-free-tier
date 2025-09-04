da94e43eb82590549948a6ea67ee04accaa23ead
yohrannes <yohrannes@gmail.com>
2025-09-04 19:51:06 -0300

    fix sa module vars

--- terrafile.tf
+++ terrafile.tf
@@ -1,6 +1,6 @@
 module "service-account" {
   source  = "yohrannes/service-account/google"
   version = "0.1.1"
-  project = var.project_id
-  enable_sa_resourceble = var.enable_sa_resource
+  project_id = var.project_id # Required
+  enable_sa_resource = var.enable_sa_resource
 }
